672f0135610f759e62ec1c7ad65fd6a806541a63
Luke Mizuhashi <no.spam@please.com>
2023-12-04 23:03:55 -0800

    reanmed

--- prepare_subsample.sh
+++ prepare_subsample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUN_ID=$(uuidgen)
-python main.py --file samples/len_1000_sample_b139366c-8c7c-4b99-9684-089c28e7edbe.json > ./logs/$RUN_ID.log
+python prepare_subsample.py --file samples/len_1000_sample_b139366c-8c7c-4b99-9684-089c28e7edbe.json > ./logs/$RUN_ID.log
 unset RUN_ID
