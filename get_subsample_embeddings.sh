af3d71ebdcd9f5ce8b89204200278dd17b44a850
Luke Mizuhashi <no.spam@please.com>
2023-12-04 23:38:57 -0800

    cleanup

--- get_subsample_embeddings.sh
+++ get_subsample_embeddings.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export RUN_ID=$(uuidgen)
-python get_subsample_embeddings.py --file subsamples/len_584_sample_D3C7AEBF-69B6-4DF7-9405-4FB31A5D962E.json # > ./logs/$RUN_ID.log
+python get_subsample_embeddings.py --file subsamples/len_584_sample_D3C7AEBF-69B6-4DF7-9405-4FB31A5D962E.json > ./embeddings/$RUN_ID.log
 unset RUN_ID
